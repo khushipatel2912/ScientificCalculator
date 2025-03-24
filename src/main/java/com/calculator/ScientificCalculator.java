f9507b49853703819ec2b04bb0fca8e5490edca2
khushipatel2912 <67056307+khushipatel2912@users.noreply.github.com>
2025-03-24 17:38:43 +0530

    Update ScientificCalculator.java

--- src/main/java/com/calculator/ScientificCalculator.java
+++ src/main/java/com/calculator/ScientificCalculator.java
@@ -37,7 +37,7 @@ public class ScientificCalculator {
         ScientificCalculator calculator = new ScientificCalculator();
 
         System.out.println("Scientific Calculator Menu:");
-        System.out.println("1. Square Root(√x)");
+        System.out.println("1. Square Root √x");
         System.out.println("2. Factorial(x!)");
         System.out.println("3. Natural Logarithm(ln(x))");
         System.out.println("4. Power Function(x^y)");
